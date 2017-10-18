48e6d9e93178b5a2414f87cf060d03756bcaff30
Fakhru Sidqi Fazari <1570060031@student.unsil.ac.id>
2017-10-18 19:33:46 +0700

    Fixed migration

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -15,9 +15,11 @@ class LineController extends Controller
         $data = json_decode($request->getContent());
         $type = $data->events[0]->type;
         if ($type == "follow") {
-            $friend = Linefriend::firstOrNew(['friend_id' => (string) $data->events[0]->source->userId]);
-            $friend->friend_id = $data->events[0]->source->userId;
-            $friend->save();
+            $chat_id = $data->events[0]->source->userId;
+            $this->sendItemInfo($chat_id, 1);
+            $friend = Linefriend::firstOrCreate(['friend_id' => (string) $data->events[0]->source->userId]);
+//            $friend->friend_id = $data->events[0]->source->userId;
+//            $friend->save();
             $chat_id = $data->events[0]->source->userId;
             $this->sendItemInfo($chat_id, 1);
         } else {
